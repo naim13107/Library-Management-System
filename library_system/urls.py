13d181de75afac76b59028139e2194e6fb00f4e1
MD.Naim-UL-Haque <naimulh644@gmail.com>
2026-02-21 03:23:11 +0600

    fixed urls

--- library_system/urls.py
+++ library_system/urls.py
@@ -20,14 +20,16 @@ from .views import api_root_view
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Library Management Api",
       default_version='v1',
-      description="Test description",
+      description="Managing Library smartly",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      contact=openapi.Contact(email="mmanagelibrary@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -41,3 +43,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
